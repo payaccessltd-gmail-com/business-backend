35f5cd44e854c6d4e9228f3dfb81fe73ba369057
Kachi Akujua <smicer66@gmail.com>
2023-07-12 19:18:39 +0100

    Changed build number

--- src/main/resources/install.sh
+++ src/main/resources/install.sh
@@ -1,3 +1,3 @@
-mkdir /opt/pay-access-api
-cp /var/lib/jenkins/workspace/PayAccess/target/payaccess-0.0.1-SNAPSHOT-${BUILD_NUMBER}.jar /opt/pay-access-api/payaccess-0.0.1-SNAPSHOT.jar
+sudo mkdir /opt/pay-access-api
+sudo cp /var/lib/jenkins/workspace/PayAccess/target/payaccess-0.0.1-SNAPSHOT-${BUILD_NUMBER}.jar /opt/pay-access-api/payaccess-0.0.1-SNAPSHOT.jar
 java -jar /opt/pay-access-api/payaccess-0.0.1-SNAPSHOT.jar
